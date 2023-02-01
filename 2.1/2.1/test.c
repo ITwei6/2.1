759b56c2f63e0a65f2c56d5c3af89b3f06911b5d
2268129437@qq.com <2268129437@qq.com>
2023-02-01 21:55:42 +0800

    作业练习

---
+++ 2.1/2.1/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int arr[100];
+//	int i;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int count1 = 0;
+//	int count2 = 0;
+//	for (i = 0; i < n-1; i++)
+//	{
+//		if (arr[i] <= arr[i + 1])
+//		{
+//			count1++;
+//		}
+//		else
+//		{
+//			count2++;
+//		}
+//	}
+//	if (count1 == n - 1 || count2 == n - 1)
+//	{
+//		printf("sorted\n");
+//	}
+//	else
+//	{
+//		printf("unsorted\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//
+//	int i;
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("xiao tao\n");
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+int main()
+{
+	while (1)
+	{
+		printf("xiao tao\n");
+	}
+	return 0;
+}
